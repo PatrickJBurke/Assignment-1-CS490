56e8de151de8f97758fa2c0670132e68a2c853c3
PatrickJBurke <44032362+PatrickJBurke@users.noreply.github.com>
2024-01-27 18:28:20 -0600

    Update problemTwo.sh

--- problemTwo.sh
+++ problemTwo.sh
@@ -12,3 +12,27 @@ if [ -f /users/paburke/output.txt ]; then
 fi
 
 #ls /users/paburke >> /users/paburke/output.txt
+
+#!/usr/bin/env bash
+
+SHARE_DIR="/users/paburke"
+
+if [[ $# -lt 1 ]];
+then
+	echo "Please enter two inputs"
+	exit
+fi
+
+if [ "$1" = "-s" ];
+then
+	echo "starting the program"
+fi
+
+if [ "$2" = "-c" ];
+then
+	echo "config selected"
+fi
+
+echo "the first variable is $1 and second variable is $2"
+
+#ls /etc > $SHARE_DIR/etc_output.txt
