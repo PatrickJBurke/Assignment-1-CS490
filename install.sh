da1b26c609a555b257736f30fef3218005376ae7
PatrickJBurke <44032362+PatrickJBurke@users.noreply.github.com>
2024-01-27 18:29:31 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,24 @@ rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 ~/miniconda3/bin/conda init zsh
 
+SHARE_DIR="/users/paburke"
+
+if [[ $# -lt 1 ]];
+then
+	echo "Please enter two inputs"
+	exit
+fi
+
+if [ "$1" = "-s" ];
+then
+	echo "starting the program"
+fi
+
+if [ "$2" = "-c" ];
+then
+	echo "config selected"
+fi
+
+echo "the first variable is $1 and second variable is $2"
+
+#ls /etc > $SHARE_DIR/etc_output.txt
